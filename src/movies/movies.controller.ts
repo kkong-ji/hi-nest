84441ef5ce5b5b34cea1eebf272a0a28c2d5d7ef
Kong ji han <wisejohn950330@gmail.com>
2022-08-25 15:52:42 +0900

    Movies Service part two

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -30,10 +30,7 @@ export class MoviesController {
 
     @Patch("/:id")
     patch(@Param('id') movieId: string, @Body() updateData) {
-        return {
-            updatedMovie: movieId,
-            ...updateData,
-        };
+        return this.moviesService.update(movieId, updateData);
     }
 
 }
