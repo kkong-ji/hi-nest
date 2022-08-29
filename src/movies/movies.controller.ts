dfa36d0e88d70674400d58ca5d139292f56d225e
Kong ji han <wisejohn950330@gmail.com>
2022-08-29 15:02:10 +0900

    DTOs and Validation part Two

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 
 @Controller('movies')
 export class MoviesController {
@@ -29,7 +30,7 @@ export class MoviesController {
     }
 
     @Patch("/:id")
-    patch(@Param('id') movieId: number, @Body() updateData) {
+    patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
         return this.moviesService.update(movieId, updateData);
     }
 
